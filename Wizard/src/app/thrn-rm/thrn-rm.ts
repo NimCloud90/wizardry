f2787e31d6e15b0e2c5bb3cc80ae7dd4c158eec6
NimCloud90 <shayleefarris@gmail.com>
2025-07-10 18:07:07 -0500

    putting the sprites in the sprite file but it's acting weird so i need to close this window and open a new one real quick (plus more setup)

---
+++ Wizard/src/app/thrn-rm/thrn-rm.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thrn-rm',
+  imports: [],
+  templateUrl: './thrn-rm.html',
+  styleUrl: './thrn-rm.css'
+})
+export class ThrnRm {
+
+}
