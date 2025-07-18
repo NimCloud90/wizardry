b14ecf3c609a8e7ca6aec69a8948a2a35e987116
NimCloud90 <shayleefarris@gmail.com>
2025-07-17 19:19:08 -0500

    more set up cause i'd rather do it now than later

---
+++ Wizard/src/app/player-menu/player-menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-menu',
+  imports: [],
+  templateUrl: './player-menu.html',
+  styleUrl: './player-menu.css'
+})
+export class PlayerMenu {
+
+}
