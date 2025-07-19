4d3886df662cdde9152a2701ba980e0c42cc6d24
NimCloud90 <shayleefarris@gmail.com>
2025-07-18 19:37:09 -0500

    got the personal player stuff sorted out

---
+++ Wizard/src/app/player-auth/player-auth.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-auth',
+  imports: [],
+  templateUrl: './player-auth.html',
+  styleUrl: './player-auth.css'
+})
+export class PlayerAuth {
+
+}
---
+++ Wizard/src/app/player-auth/player-menu/player-settings/player-settings.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-settings',
+  imports: [],
+  templateUrl: './player-settings.html',
+  styleUrl: './player-settings.css'
+})
+export class PlayerSettings {
+
+}
