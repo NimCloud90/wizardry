b1749f650c7588eaf69e396bbf25a44da9344d1a
NimCloud90 <shayleefarris@gmail.com>
2025-09-11 19:00:02 -0500

    good riddance

--- wizard-back/models/routes/savesRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { saveGame, loadGame } from '../controllers/saveController';
-
-const router = Router();
-router.post('/save', saveGame);
-router.get('/load/:playerId', loadGame);
-
-export default router;
